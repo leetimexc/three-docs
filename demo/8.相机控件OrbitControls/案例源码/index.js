2fe4f2f33462ce0f663f03d4c2487a5f425d0afd
li_shuai <506507881@qq.com>
2025-09-11 22:38:44 +0800

    feat: 相机控件OrbitControls

---
+++ demo/8.相机控件OrbitControls/案例源码/index.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+// 1.创建场景
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// 2.创建相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+scene.add(camera);
+
+// 3.创建渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+// 4.添加相机控件
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', function () {
+    renderer.render(scene, camera); //执行渲染操作
+});//监听鼠标、键盘事件
